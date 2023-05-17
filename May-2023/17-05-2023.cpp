395b4270ed9beffb35c13789f5f28841e9c01ede
Ajay Kushwaha <ak383834@gmail.com>
2023-05-17 16:22:09 +0530

    solved

--- May-2023/17-05-2023.cpp
+++ May-2023/17-05-2023.cpp
@@ -1,39 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-int pairSum(ListNode* head) {
-ListNode* slow = head;
-ListNode* fast = head;
-int maxVal = 0;
-while(fast && fast -> next)
-{
-slow = slow -> next;
-fast = fast -> next -> next;
-}
-ListNode *nextNode, *prev = NULL;
-while (slow) {
-nextNode = slow->next;
-slow->next = prev;
-prev = slow;
-slow = nextNode;
-}
-while(prev)
-{
-maxVal = max(maxVal, head -> val + prev -> val);
-prev = prev -> next;
-head = head -> next;
-}
-return maxVal;
-}
-};
+
+    int isPossible(int n, int m, string s){
+        // code here
+        int side=0,niche=0,maxSide=0,minSide=0,maxNiche=0,minNiche=0;
+        for(auto x:s){
+            if(x=='R')side++;
+            else if(x=='L')side--;
+            else if(x=='U')niche--;
+            else niche++;
+            maxSide=max(maxSide,side);
+            minSide=min(minSide,side);
+            maxNiche=max(maxNiche,niche);
+            minNiche=min(minNiche,niche);
+        }
+        if(maxSide+abs(minSide)+1>m)return 0;
+        if(maxNiche+abs(minNiche)+1>n)return 0;
+        return 1;
+    }
